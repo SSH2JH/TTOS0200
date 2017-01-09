811f94243e47a6648d5f837f7ceceab7859ba7e9
juuso <juuso@hmm>
2017-01-09 22:20:04 +0200

    Updates!

--- Lotto/Program.cs
+++ Lotto/Program.cs
@@ -4,7 +4,16 @@ namespace Lotto {
 	class Program {
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Random rnd = new Random();
+			int[] Numbers = new int[7];
+
+			for (int i = 0; i < 7; i++) {
+				Numbers[i] = rnd.Next(1,40);
+			}
+
+			for (int i = 0; i < 7; i++) {
+				Console.WriteLine(Numbers[i]);
+			}
 		}
 	}
 }
