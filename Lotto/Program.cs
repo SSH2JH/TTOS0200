f984dfe1176c75cbce6b821e2bb1b2a0be166931
juuso <juuso@ascasvsa>
2017-01-10 14:20:13 +0200

    more updates

--- Lotto/Program.cs
+++ Lotto/Program.cs
@@ -1,18 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lotto {
 	class Program {
 		static void Main(string[] args)
 		{
+			const int a = 7;
 			Random rnd = new Random();
-			int[] Numbers = new int[7];
-
-			for (int i = 0; i < 7; i++) {
-				Numbers[i] = rnd.Next(1,40);
+			int[] Numbers = new int[40];
+			int[] WinNumbers = new int[a];
+			int apu;
+			for (int i = 0; i < Numbers.Length; i++) {
+				Numbers[i] = i;
 			}
-
-			for (int i = 0; i < 7; i++) {
-				Console.WriteLine(Numbers[i]);
+			for (int i = 0; i < a; i++) {
+				apu = rnd.Next(Numbers[39]);
+				if (WinNumbers.Contains(apu)) {
+					i--;
+				} else {
+					WinNumbers[i] = apu;
+				}
+			}
+			Array.Sort(WinNumbers);
+			for (int i = 0; i < a; i++) {
+				Console.WriteLine(WinNumbers[i]);
 			}
 		}
 	}
