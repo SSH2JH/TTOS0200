9d462b7856715b11e1e4ed5a2ca47d96f0854692
K8960 <K8960@TIET-334-WS-05>
2017-01-10 16:27:37 +0200

    Uppaan harj 1

---
+++ Harjoitus1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace Harjoitus1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Please enter a number >");
+			int a = int.Parse(Console.ReadLine());
+
+			switch (a) {
+				case 1:
+					Console.WriteLine("You entered number one!");
+					break;
+				case 2:
+					Console.WriteLine("You entered number two!");
+					break;
+				case 3:
+					Console.WriteLine("You entered number three!");
+					break;
+				default:
+					Console.WriteLine("You entered some other number!");
+					break;
+			}
+		}
+	}
+}
