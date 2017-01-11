437d252b04d7a055f3561efa76b9b662d1e9238d
juuso <juuso@DESKTOP-H8ND6HS>
2017-01-11 15:33:55 +0200

    Testing powershell commit

---
+++ Harjoitus6/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: Tehtävä 5
+ * Kuvaus:
+ *		Auton kulutus on 7.02 litraa 100 kilometrin matkalla ja bensan hinta on 1.595 Euroa.
+ *		Tee ohjelma, joka tulostaa ajetulla matkalla (kysytään käyttäjältä) kuluvan bensan määrän
+ *		sekä bensaan menevän rahan määrän.
+ *		
+ *		Esimerkkitoiminta:
+ *			Anna matka > 250 [Enter]
+ *			Bensaa kuluu 17,55 litraa, kustannus 27,99 euroa
+ * Pvm: 11.1.2016 
+ */
+
+using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace Harjoitus6 {
+	class Program {
+		static void Main(string[] args)
+		{
+			Console.Write("Please enter the distance (km) >");
+			int Distance = int.Parse(Console.ReadLine());
+			int stuff;
+
+			
+
+			Console.WriteLine();
+		}
+	}
+}
