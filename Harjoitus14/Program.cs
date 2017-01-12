ec40f7b12712e861ae5a95fa9e6ec8ecc31b490b
K8960 <K8960@TIET-334-WS-05>
2017-01-12 13:40:07 +0200

    Task 14 is started

---
+++ Harjoitus14/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: Tehtävä 14
+ * Kuvaus:
+ *		Kirjoita ohjelma, joka pyytää käyttäjältä opiskelijoiden
+ *		arvosanat 0-5 ohjelmointi-opintojaksosta (voit itse päättää lopetusehdon).
+ *		Tulosta arvosanajakauma käyttäen tähtimerkkejä seuraavasti:
+ *		
+ *		Arvosanajakauma:
+ *		0:
+ *		1:****
+ *		2:**
+ *		3:******
+ *		4:*****
+ *		5:** 
+ * Pvm: 12.1.2016 
+ */
+
+using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace Harjoitus14
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
