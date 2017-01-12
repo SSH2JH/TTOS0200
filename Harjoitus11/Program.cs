62160bf945af2eb8fad6360d0e68fb2ad3548ce9
K8960 <K8960@TIET-334-WS-05>
2017-01-12 12:36:51 +0200

    Task11 finished and commented

---
+++ Harjoitus11/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: Tehtävä 11
+ * Kuvaus:
+ *		Tee kahden sisäkkäisen for-silmukan avulla ohjelma,
+ *		joka tulostaa seuraavanlaisen kuvion:
+ *		*
+ *		**
+ *		***
+ *		****
+ *		*****
+ *		
+ *		Käyttäjä antaa syötteenä tähtirivien lukumäärän.
+ *		Yllä olevassa esimerkissä käyttäjä on syöttänyt luvun 5.
+ * Pvm: 12.1.2016 
+ */
+
+using System;
+
+namespace Harjoitus11
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Kysyy käyttäjältä luku
+			int Rounds;
+			Console.Write("Please enter a number >");
+			Rounds = int.Parse(Console.ReadLine());
+
+			// Pyöräyttää nested loop tekniikalla käyttäjän antaman luvun perusteella
+			for(int i = 0; i < (Rounds); i++) {
+				for(int j = 0; j < (i+1); j++) {
+					Console.Write("*"); // Tämä pyörähtää ylemmän silmukan kierrosten verran
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
