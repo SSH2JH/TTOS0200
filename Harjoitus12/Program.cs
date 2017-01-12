e92d165c4d334e70978f6fef4573ddc85d8aa41e
K8960 <K8960@TIET-334-WS-05>
2017-01-12 13:15:45 +0200

    Task 12 finished and commented

---
+++ Harjoitus12/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: Tehtävä 12
+ * Kuvaus:
+ *		Tee ohjelma, joka kysyy käyttäjältä 5 kokonaislukua.
+ *		Luvut tulee sijoittaa taulukkoon.
+ *		Ohjelman tulee tulostaa annetut luvut käänteisessä järjestyksessä.
+ *		
+ *		Anna Luku > 1 [Enter]
+ *		Anna Luku > 2 [Enter]
+ *		Anna Luku > 3 [Enter]
+ *		...
+ *		...
+ *		Luvut ovat: 3,2,1
+ * Pvm: 12.1.2016 
+ */
+
+using System;
+
+namespace Harjoitus12
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Luodaan integer-taulukko ja lukitaan sen pituus viiteen
+			int[] Numbers = new int[5];
+			// Täytetään taulukko käyttäjän syötteellä
+			for(int i = 0; i < 5; i++) {
+				Console.Write("Please enter the {0}. number >", (i+1));
+				Numbers[i] = int.Parse(Console.ReadLine());
+			}
+			// Tulostetaan käyttäjän antama syöte vastakkaisessa järjestyksessä
+			Console.Write("Numbers are in reverse: ");
+			for (int i = (Numbers.Length - 1); i >= 0; i--) {
+				Console.Write("{0} ", Numbers[i]);
+			}
+			Console.WriteLine();
+		}
+	}
+}
