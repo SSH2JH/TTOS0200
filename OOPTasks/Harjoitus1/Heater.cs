6d0afee860f94b0776b371fb6dd0c10f8ec318e4
juuso <juuso@DESKTOP-H8ND6HS>
2017-01-16 13:34:42 +0200

    created new tasks folder and started task1

---
+++ OOPTasks/Harjoitus1/Heater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace OOP.Classes {
+	public class Heater {
+		public bool IsOn { get; set; }
+		public float Termperature { get; set; }
+
+		// Kosteus voi olla välillä 0-100%
+		float humidity;
+		public float Humidity {
+			get { return humidity; }
+			set {
+				humidity = value;
+			}
+		}
+	}
+}
---
+++ OOPTasks/Harjoitus1/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: OOP - Tehtävä 1
+ * Kuvaus:
+ *		Tee ohjelma, joka alustaa sovellukseen käyttöö seuraavan
+ *		taulukon arvot = [1,2,33,44,55,68,77,96,100].
+ *		Käy sovelluksessa taulukko läpi ja tulosta ruutuun "HEP"-sana
+ *		aina kun taulukossa oleva luku on parillinen.
+ * Pvm: 16.1.2017
+ */
+
+using System;
+using OOP.Classes;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace OOP.Tasks {
+	class Program {
+		static void Main(string[] args)
+		{
+			TestHeater();
+		}
+		static void TestHeater()
+		{
+			// Luodaan kiuas
+			Heater heater = new OOP.Classes.Heater();
+			// Laitetaan päälle
+			heater.IsOn = true;
+			heater.Termperature = 90f;
+			heater.Humidity = 50f;
+
+			// Näytetään konsolilla
+			Console.WriteLine("Heater is on {0}", heater.IsOn);
+			Console.WriteLine("Heater's temperature {0}", heater.Termperature);
+			Console.WriteLine("Heater's humidity {0}", heater.Humidity);
+		}
+	}
+}
