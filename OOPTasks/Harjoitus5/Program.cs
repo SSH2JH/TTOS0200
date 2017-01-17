661a9859a6999a1254733eb4e69b217d417bf1c8
juuso <juuso@DESKTOP-H8ND6HS>
2017-01-17 21:52:47 +0200

    task5 finished and commented

---
+++ OOPTasks/Harjoitus5/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: OOP - Tehtävä 5
+ * Kuvaus:
+ *		Suunnittele UML-editorilla Opiskelija-luokka,
+ *		joka sisältää opiskelijalle tyypillisiä tietoja ja toimintoja.
+ *		Tee uusi projekti. Ohjelmoi Opiskelija-luokka sekä toteuta pääohjelma,
+ *		joka luo muutamia opiskelijoita ja tallentaa opiskelijat taulukkoon (esim. 5 opiskelijaa).
+ *		Tulosta taulukossa olevien opiskelijoiden tiedot käyttämällä toistorakennetta.
+ * Pvm: 17.1.2017
+ */
+
+using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace OOP.Tasks {
+	class Program {
+		static void Main(string[] args)
+		{
+			StudentDatabase();
+		}
+		static void StudentDatabase()
+		{
+			// Määrittää taulukon koon
+			const int quantity = 5;
+
+			// Aloittaa olion taulukon ja sen sisällöt
+			Student[] student = new Student[quantity];
+			for (int i = 0; i < quantity; i++) {
+				student[i] = new Student();
+			}
+
+			// Asettaa olion parametreille arvot
+
+			// 1. student
+			student[0].Name = "Tony Russel";
+			student[0].StudentId = "001";
+			student[0].MainSubject = "Programming";
+			student[0].IsActive = true;
+			// 2. student
+			student[1].Name = "Lucy O'Doherty";
+			student[1].StudentId = "002";
+			student[1].MainSubject = "Psychology";
+			student[1].IsActive = true;
+			// 3. student
+			student[2].Name = "Matt Mathewson";
+			student[2].StudentId = "003";
+			student[2].MainSubject = "Math";
+			student[2].IsActive = false;
+			// 4. student
+			student[3].Name = "Ned Lannister";
+			student[3].StudentId = "004";
+			student[3].MainSubject = "History";
+			student[3].IsActive = true;
+			// 5. student
+			student[4].Name = "Some Random Lass";
+			student[4].StudentId = "005";
+			student[4].MainSubject = "Programming";
+			student[4].IsActive = true;
+
+			// Tulostaa tietokannan, siis taulukon
+			for (int i = 0; i < quantity; i++) {
+				student[i].PrintStudent();
+			}
+		}
+	}
+}
---
+++ OOPTasks/Harjoitus5/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP.Tasks {
+	public class Student {
+
+		// Parametrit nini,
+		public string Name { get; set; }
+		// id,
+		public string StudentId { get; set; }
+		// pääaine,
+		public string MainSubject { get; set; }
+		// onko läsnä lukukaudessa
+		public bool IsActive { get; set; }
+
+		// Tulostaa parametrit
+		public void PrintStudent()
+		{
+			Console.WriteLine("\nName: {0}", Name);
+			Console.WriteLine("ID: {0}", StudentId);
+			Console.WriteLine("Main Subject: {0}", MainSubject);
+			Console.WriteLine("Attending: {0}", IsActive);
+			Console.WriteLine("--------------------------------");
+		}
+	}
+}
