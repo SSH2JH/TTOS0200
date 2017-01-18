dc36b3de88dd1283c157681a949e438019af3ab0
juuso <juuso@DESKTOP-H8ND6HS>
2017-01-18 15:07:06 +0200

    Lab 4 - Task1 finished and commented

---
+++ OOPTasks-Labra4/Harjoitus1/Elevator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OOP.Tasks {
+	class Elevator {
+		int floor = 1;
+		public int CurrentFloor
+		{
+			get { return floor; }
+		}
+
+		private string SwapValue(int newFloor)
+		{
+			// Tarkistaa että syöte on suurempi kuin nolla ja pienempi, tai yhtäsuuri kuin 5
+			if (newFloor <= 5 && newFloor > 0) {
+				floor = newFloor;
+				return ("New floor is: " + CurrentFloor);
+			}
+			else {
+				return ("Your value was out of reach!\n" + "Your current floor is: " + CurrentFloor);
+			}
+		}
+
+		public string ChangeFloor(string input)
+		{
+			int inputNumeric;
+			bool isNumeric;
+			// Tarkistetaan käyttäjän syötteen nuumeerius
+			isNumeric = int.TryParse(input, out inputNumeric);
+
+			if (isNumeric == true) {
+				// Tulostetaan uusi kerros ja vaihdetaan nykyisen kerroksen arvoa
+				return SwapValue(inputNumeric);
+			}
+			else {
+				// Yleinen virheviesti
+				return ("Invalid value!");
+			}
+		}
+	}
+}
---
+++ OOPTasks-Labra4/Harjoitus1/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: OOP Labra 4 - Tehtävä 1
+ * Kuvaus:
+ *		Tehtävänäsi on ohjelmoida Dynamon hissin kerroksen ohjaukseen liittyvä säädin.
+ *		Toteutetun ohjelman tulee pystyä kysymään käyttäjältä haluttu kerros ja 
+ *		siirtämään hissi haluttuun kerrokseen (tässä tapauksessa ohjelma kertoo
+ *		käyttäjälle missä kerroksessa hissi on). Muista, että Dynamon hissi voi olla vain kerroksissa 1-5.
+ *		Käytä Hissi-luokassa get- ja set-aksessoreita suojamaan olion tila.
+ * Pvm: 17.1.2017
+ */
+
+using System;
+
+namespace OOP.Tasks {
+	public class Program {
+		static void Main(string[] args)
+		{
+			ElevatorFunc();
+		}
+
+		static void ElevatorFunc()
+		{
+			// Asetetaan muuttujat
+			Elevator elevator = new Elevator();
+			// Tulostetaan nykyinen kerros
+			Console.WriteLine("Your current floor is {0}", elevator.CurrentFloor);
+
+			// Ohjelman päälooppi
+			while (true) {
+				// Kysytään käyttäjältä arvot
+				Console.Write("Please, give a new floor number (-1 to exit) >");
+				string input = Console.ReadLine();
+				if (input != "-1") {
+					Console.WriteLine(elevator.ChangeFloor(input));
+				}
+				else {
+					// Ohjelman suoritus päättyy
+					Console.WriteLine("Exiting...");
+					break;
+				}
+			}
+		}
+	}
+}
