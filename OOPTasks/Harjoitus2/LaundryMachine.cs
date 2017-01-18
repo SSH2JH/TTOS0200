9888d6497e45a8a7ddccdd8bc9cd2691fc767a03
juuso <juuso@DESKTOP-H8ND6HS>
2017-01-18 12:12:43 +0200

    Task2 is finished and nearly commented

---
+++ OOPTasks/Harjoitus2/LaundryMachine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP.Tasks {
+	class LaundryMachine {
+		public bool IsOn { get; set; }
+		public int RotationSpeed { get; set; }
+		public int ProgramLenght { get; set; }
+		public float Temperature { get; set; }
+
+		public string PrintProgram(LaundryMachine x)
+		{
+			// x edustaa tuntematonta oliota joka syötetään pääohjelmasta
+			return String.Format("Is on: {0}, RPM: {1}, Program Time: {2} min, Temperature {3} C",
+				x.IsOn, x.RotationSpeed, x.ProgramLenght, x.Temperature);
+		}
+	}
+}
---
+++ OOPTasks/Harjoitus2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: OOP - Tehtävä 2
+ * Kuvaus:
+ *		
+ * Pvm: 18.1.2017
+ */
+
+using System;
+
+namespace OOP.Tasks {
+	class Program {
+		static void Main(string[] args)
+		{
+			LaundryMachineFunc();
+		}
+
+		static void LaundryMachineFunc()
+		{
+			LaundryMachine lMachine = new LaundryMachine();
+			lMachine.IsOn = true;
+			lMachine.RotationSpeed = 1400;
+			lMachine.ProgramLenght = 90;
+			lMachine.Temperature = 60;
+
+			Console.WriteLine(lMachine.PrintProgram(lMachine));
+
+		}
+	}
+}
