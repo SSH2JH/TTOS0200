83028a0a927f511851a552ff35b74dcadb91172c
juuso <juuso@DESKTOP-H8ND6HS>
2017-01-22 18:53:30 +0200

    Task3 finished and commented

---
+++ OOPTasks-Labra4/Harjoitus3/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP.Tasks {
+
+	// Pääluokka
+	class Employee {
+		public string Name { get; set; }
+		public string Profession { get; set; }
+		public float Salary { get; set; }
+
+		public Employee()
+		{
+		}
+
+		public override string ToString()
+		{
+			return Name + ", " + Profession + ", " + Salary + " euros/h";
+		}
+	}
+
+	// Periytyvä luokka
+	class Boss : Employee {
+		public string Car { get; set; }
+		public float Bonus { get; set; }
+
+		public Boss()
+		{
+		}
+
+		public override string ToString()
+		{
+			return base.ToString() + " + " + Bonus + " euros/month" + ", " + Car;
+		}
+	}
+}
---
+++ OOPTasks-Labra4/Harjoitus3/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: OOP Labra 4 - Tehtävä 3
+ * Kuvaus:
+ *		Ohjelmassa tulee pystyä käsittelemään työntekijöiden tietoja (Employee).
+ *		Työntekijöiden osalta seuraavia tietoa pitää pystyä käsittelemään:
+ *		työntekijän nimi (Name), työntekijän ammatti (Profession) ja palkka (Salary).
+ *		Samassa ohjelmassa pitää pystyä käsittelemään myös johtajien tietoja (Boss),
+ *		heillä on edellisten lisäksi myös auto (Car) ja palkkabonus (Bonus).
+ *		
+ *		Tutki tehtävän tavoitetta/kerrontaa ja toteuta tarvittavat UML-luokkakaaviot.
+ *		Toteuta tämän jälkeen vaaditut luokat, luo ja käytä olioita pääohjelmasta.
+ *		Tulosta vaadittujen luokkien olioiden tietoja output-ikkunaan.
+ *		Tietoja ei tarvitse kysyä sovelluksen käyttäjältä, vaan voit alustaa ne suoraan pääohjelmassa. 
+ * Pvm: 22.1.2017
+ */
+
+using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace OOP.Tasks {
+	class Program {
+		static void Main(string[] args)
+		{
+			WorkPlaceStaff();
+		}
+
+		static void WorkPlaceStaff()
+		{
+			Console.WriteLine("Name, Profession, Salary, Bonus, Car");
+			Console.WriteLine("-----------------------------------------------");
+
+			Employee employee1 = new Employee();
+			employee1.Name = "Henri Jokinen";
+			employee1.Profession = "Programmer";
+			employee1.Salary = 14.82f;
+			Console.WriteLine(employee1.ToString());
+
+			Console.WriteLine("-----------------------------------------------");
+
+			Employee employee2 = new Employee();
+			employee2.Name = "Keijo Nieminen";
+			employee2.Profession = "Senior Programmer";
+			employee2.Salary = 19.82f;
+			Console.WriteLine(employee2.ToString());
+
+			Console.WriteLine("-----------------------------------------------");
+
+			Boss boss1 = new Boss();
+			boss1.Name = "Matti Järvinen";
+			boss1.Profession = "CEO";
+			boss1.Car = "Audi";
+			boss1.Salary = 30.45f;
+			boss1.Bonus = 650.12f;
+			Console.WriteLine(boss1.ToString());
+
+			Console.WriteLine("-----------------------------------------------");
+
+			Boss boss2 = new Boss();
+			boss2.Name = "Tero Syvänne";
+			boss2.Profession = "Project Manager";
+			boss2.Car = "Mercedes Benz";
+			boss2.Salary = 28.45f;
+			boss2.Bonus = 350.12f;
+			Console.WriteLine(boss2.ToString());
+
+			Console.WriteLine("-----------------------------------------------");
+		}
+	}
+}
