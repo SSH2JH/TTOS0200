cb71f3cb541a4afa8723c7d8d865bbe6e6ffaebe
juuso <juuso@DESKTOP-H8ND6HS>
2017-01-23 01:13:10 +0200

    Task6 finished

---
+++ OOPTasks-Labra4/Harjoitus6/BookShelfItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace OOP.Tasks {
+	class BookShelfItem {
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public bool IsUseful { get; set; }
+
+		public override string ToString()
+		{
+			return Name + ": Age " + Age + " years, Useful " + IsUseful;
+		}
+	}
+	class Disk : BookShelfItem {
+		public string Type { get; set; }
+
+		private int rating;
+		public int Rating
+		{
+			get { return rating; }
+			set {
+				rating = value;
+				if (rating < 0) {
+					rating = 0;
+				} else if (rating > 10) {
+					rating = 10;
+				}
+			}
+		}
+
+		public override string ToString()
+		{
+			string tmp = string.Format("\n\tType {0}, Rating {1}/10", Type, Rating);
+			return base.ToString() + tmp;
+		}
+
+	}
+	class Book : BookShelfItem {
+		public string Genre { get; set; }
+		public int Pages { get; set; }
+
+		public override string ToString()
+		{
+			string tmp = string.Format("\n\tGenre {0}, Pages {1}", Genre, Pages);
+			return base.ToString() + tmp;
+		}
+	}
+}
---
+++ OOPTasks-Labra4/Harjoitus6/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: OOP Labra 4 - Tehtävä 6
+ * Kuvaus:
+ *		ICT-opiskelijan kirjahyllystä löytyy vaikka mitä erilaisia tavaroita:
+ *		kirjoja, lehtiä, cd-levyjä, dvd-levyjä, bluray-levyjä, puhelimia, tabletteja, läppäreitä, ... jne.
+ *		Pohdi UML-kaaviota käyttäen millaisia luokkarakenteita
+ *		(ainakin luokkien ja ominaisuuksien osalta) esiintyy ja käytä apuna perintää jos tavaroille
+ *		löytyy yhteisiä ominaisuuksia. Toteuta tämän jälkeen muutamia luokkia,
+ *		joissa perintää esiintyy. Tee myös pääohjelma, jossa käytät tekemiäsi luokkia ja luot olioita.
+ * Pvm: 23.1.2017
+ */
+
+using System;
+
+namespace OOP.Tasks {
+	class Program {
+		static void Main(string[] args)
+		{
+			BookShelfDB();
+		}
+		static void BookShelfDB()
+		{
+			Disk item1 = new Disk();
+			item1.Name = "Metallica album";
+			item1.Age = 5;
+			item1.IsUseful = true;
+			item1.Type = "Music - Rock";
+			item1.Rating = 10;
+			Console.WriteLine(item1.ToString());
+
+			Console.WriteLine("\n-----------------------------------------------");
+
+			Book item2 = new Book();
+			item2.Name = "Eragon";
+			item2.Age = 5;
+			item2.IsUseful = true;
+			item2.Genre = "Fantasy";
+			item2.Pages = 600;
+			Console.WriteLine(item2.ToString());
+		}
+	}
+}
