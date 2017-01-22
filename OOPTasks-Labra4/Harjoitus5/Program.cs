5722aa4d7794b5977859fe47124eac01c7b977cc
juuso <juuso@DESKTOP-H8ND6HS>
2017-01-23 00:25:01 +0200

    Task5 has been finished and commented

---
+++ OOPTasks-Labra4/Harjoitus5/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: OOP Labra 4 - Tehtävä 5
+ * Kuvaus:
+ *		Tehtävässä tulee toteuttaa C#-ohjelma radion perustoimintojen testaamiseen.
+ *		
+ *		Kannettavassa radiossa on vain kolme säädintä: päälle/pois-kytkin,
+ *		äänen voimakkuuden säädin (arvot 0, 1, 2,..., 9) ja kuunneltavan kanavan taajuusvalinta (2000.0 - 26000.0).
+ *		
+ *		Laadi luokka kannettavan radion toteutukseksi. Käytä tekemääsi luokkaa pääohjelmasta käsin,
+ *		eli testaile radion toimivuutta erilaisilla voimakkuuden ja taajuuden arvoilla.
+ *		Jätä asetus- ja tulostuslauseet näkyville pääohjelmaan, jotta radio-olion käyttö voidaan todentaa.
+ * Pvm: 22.1.2017
+ */
+
+using System;
+
+namespace OOP.Tasks {
+	class Program {
+		static void Main(string[] args)
+		{
+			HandRadio();
+		}
+		static void HandRadio()
+		{
+			Radio radio = new Radio();
+			radio.IsOn = true;
+			radio.Volume = 20;
+			radio.Frequency = 12000.3;
+			Console.WriteLine(radio.ToString());
+
+			Console.WriteLine("-------------------------------");
+
+			radio.IsOn = false;
+			radio.Volume = 20;
+			radio.Frequency = 19000.20;
+			Console.WriteLine(radio.ToString());
+
+			Console.WriteLine("-------------------------------");
+
+			radio.IsOn = true;
+			radio.Volume = 5;
+			radio.Frequency = 50000.1;
+			Console.WriteLine(radio.ToString());
+		}
+	}
+}
---
+++ OOPTasks-Labra4/Harjoitus5/Radio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace OOP.Tasks {
+	class Radio {
+		// Radio on/off
+		public bool IsOn { get; set; }
+		
+		// Controls volume
+		private int volume;
+		public int Volume
+		{
+			get { return volume; }
+			set {
+				volume = value;
+				if (IsOn == false || volume < 0) {
+					volume = 0;
+				} else if (volume > 9) {
+					volume = 10;
+				}
+			}
+		}
+
+		// Controls frequency
+		private double freq;
+		public double Frequency
+		{
+			get { return freq; }
+			set {
+				freq = value;
+				if (freq < 2000.0) {
+					freq = 2000.0;
+				} else if (freq > 26000.0) {
+					freq = 26000.0;
+				}
+			}
+		}
+
+		// Outputs string
+		public override string ToString()
+		{
+			string tmp = string.Format("Turned on: {0}, Volume: {1}/10, Analog frequency: {2:0.0} Hz", IsOn, Volume, Frequency);
+			return tmp;
+		}
+	}
+}
