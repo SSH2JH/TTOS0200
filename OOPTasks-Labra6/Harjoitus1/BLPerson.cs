c2f3aacb9fcd4f8229d7aecccd199f30287eb66f
juuso <juuso@DESKTOP-H8ND6HS>
2017-01-25 15:01:36 +0200

    finished lab6 task1

---
+++ OOPTasks-Labra6/Harjoitus1/BLPerson.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Tasks.T1 {
+	/// <summary>
+	/// This class contains person poperties
+	/// </summary>
+	class Person {
+		public string FirstName { get; set; }
+		public string Lastname { get; set; }
+		public string PersonID { get; set; }
+
+		public override string ToString()
+		{
+			return FirstName + " " + Lastname + " " + PersonID;
+		}
+	}
+	/// This class holds person information in a collection
+	class Persons {
+		// properties
+		private List<Person> persons;
+		public List<Person> PersonList {
+			get { return persons; }
+		}
+		public Persons()
+		{
+			persons = new List<Person>();
+		}
+		public void AddPerson(Person person)
+		{
+			persons.Add(person);
+		}
+		public Person GetPerson(int index)
+		{
+			if (index < persons.Count) {
+				return persons.ElementAt(index);
+			} else {
+				return null;
+			}
+		}
+		public Person GetPersonID(string id)
+		{
+			foreach (Person person in persons) {
+				if (person.PersonID == id) {
+					return person;
+				}
+			}
+			return null;
+		}
+	}
+}
--- OOPTasks-Labra6/Harjoitus1/Program.cs
+++ OOPTasks-Labra6/Harjoitus1/Program.cs
@@ -4,11 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOP.Tasks {
+namespace OOP.Tasks.T1 {
 	class Program {
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			TestPersonRegistry();
+		}
+		static void TestPersonRegistry()
+		{
+			// Creating few test persons
+			Persons crowd = new Persons();
+			Person prs1 = new Person { FirstName = "Micael", Lastname = "Landry", PersonID = "131232-1212A" };
+			Person prs2 = new Person { FirstName = "Lucy", Lastname = "Russel", PersonID = "121311-112312" };
+			Person prs3 = new Person { FirstName = "Creature", Lastname = "Cat", PersonID = "131231-23543B" };
+			// Add people to crowd
+			crowd.AddPerson(prs1);
+			crowd.AddPerson(prs2);
+			crowd.AddPerson(prs3);
+
+			foreach (Person x in crowd.PersonList) {
+				Console.WriteLine("{0}", x.ToString());
+			}
+			// TODO ask the client for person id, search it and print it (FINISHED)
+			Console.Write("Please enter person ID >");
+			string a = Console.ReadLine();
+			Console.WriteLine(String.Format("\nSearch result: " + crowd.GetPersonID(a)));
 		}
 	}
 }
