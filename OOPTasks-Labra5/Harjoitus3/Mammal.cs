89ee0de7a548f1202da049bd9fdce3ba2dc2cb6d
juuso <juuso@DESKTOP-H8ND6HS>
2017-01-25 02:32:31 +0200

    task3 finished

---
+++ OOPTasks-Labra5/Harjoitus3/Mammal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace OOP.Tasks {
+	abstract class Mammal {
+		public int Age { get; set; }
+
+		public Mammal(int age)
+		{
+			Age = age;
+		}
+		public abstract string Move();
+	}
+	class Human : Mammal {
+		public float Weight { get; set; }
+		public float Height { get; set; }
+		public string Name { get; set; }
+
+		public Human(string name, float weight, float height, int age)
+			: base (age)
+		{
+			Weight = weight;
+			Height = height;
+			Name = name;
+		}
+		public override string Move()
+		{
+			return "I move!";
+		}
+		public void Grow()
+		{
+			Age++;
+		}
+	}
+	class Baby : Human {
+		public string Diaper { get; set; }
+		public Baby(string diaper, string name, float weight, float height, int age)
+			:base (name, weight, height, age)
+		{
+			Diaper = diaper;
+		}
+		public override string Move()
+		{
+			return "I crawl!";
+		}
+	}
+	class Adult : Human {
+		public string Car { get; set; }
+		public Adult(string car, string name, float weight, float height, int age)
+			: base (name, weight, height, age)
+		{
+			Car = car;
+		}
+		public override string Move()
+		{
+			return "I walk!";
+		}
+	}
+}
---
+++ OOPTasks-Labra5/Harjoitus3/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: OOP Labra 5 - Tehtävä 3
+ * Kuvaus:
+ *		Tehtävässä tulee toteuttaa muutamia luokkia, joiden avulla testataan abstraktien metodien toimivuutta.
+ *		Luo abstrakti luokka Nisakas, jolla on Ika-ominaisuus sekä Liiku-metodi, jonka toteutus on jätetty toteuttamatta (abstrakti metodi).
+ *		
+ *		Peri Nisakas-luokasta Ihminen-luokka, jolla on ominaisuuksina ihmiseen yleisesti liittyviä ominaisuuksia (paino, pituus, nimi).
+ *		Lisää Ihminen-luokkaan metodit: Liiku ja Kasva, joista ensimmäinen tulostaa ruutuun "liikun" ja jälkimmäinen lisää ihmisen ikää yhdellä vuodella.
+ *		Huomioi, että Liiku-metodi ja Ikä-ominaisuus on Nisakas-luokassa
+ *		
+ *		Peri Ihminen-luokasta Vauva- ja Aikuinen-luokat. Ylikirjoita Vauva-luokassa yliluokan Liiku-metodi tulostamaan "konttaa".
+ *		Ylikirjoita Aikuinen-luokassa myös sama metodi ja laita se tulostamaan "kävelee". Lisää aikuiselle ominaisuus auto (String). Lisää vauvalle ominaisuus vaippa (String).
+ *		
+ *		Toteuta pääohjelmassa muutamia ihmisiä, aikuisia ja vauvoja. Tulostele olioiden tietoja konsolille.
+ * Pvm: 25.1.2017
+ */
+
+using System;
+
+namespace OOP.Tasks {
+	class Program {
+		static void Main(string[] args)
+		{
+			Human Mej = new Human("Mej", 68, 165, 20);
+			Baby Elina = new Baby("UltraDiaper", "Elina", 7, 40, 1);
+			Adult Genji = new Adult("Ferrari", "Genji", 70, 170, 27);
+
+			Console.WriteLine("{0} yells \"{1}\"", Mej.Name, Mej.Move());
+			Console.WriteLine("{0} shouts \"{1}\"", Genji.Name, Genji.Move());
+			Console.WriteLine("{0} screams \"{1}\"", Elina.Name, Elina.Move());
+
+			Console.WriteLine("\n{0} the diaper is {1} and height is {2}", Elina.Name, Elina.Diaper, Elina.Height);
+			Console.WriteLine("{0} car is {1} and is {2} year old", Genji.Name, Genji.Car, Genji.Age);
+			Console.WriteLine("{0} weight is {1} and height {2}", Mej.Name, Mej.Weight, Mej.Height);
+		}
+	}
+}
