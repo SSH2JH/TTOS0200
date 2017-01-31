2375fa7f4c59762471344bbd29a9762236f5f67f
juuso <juuso@DESKTOP-H8ND6HS>
2017-02-01 00:06:22 +0200

    Created placeholder for labra7 - task2

---
+++ OOPTasks-Labra7/Harjoitus2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: OOP Labra 7 - Tehtävä 1 PLACEHOLDER
+ * Kuvaus:
+ *		 Tee ohjelma, joka lukee yksinkertaisesta tekstitiedosto nimet.txt henkilöitten nimiä
+ *		 ja kertoo montako nimeä löytyy ja montako kertaa kukin nimi esiintyy.
+ *		 Kopioi (tai luo itse vastaava tiedosto) D:\Temp -kansioon ja ohjelmoi koodisi tarkistamaan onko em.hakemistossa tiedostoa.Käytä File.Exist-metodia.
+ *		 Huomioi myös muut mahdolliset poikkeukset, joita tiedoston käsittely voi aiheuttaa. 
+ * Pvm: 1.2.2017
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Tasks {
+	class Program {
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World! This task is a placeholder!");
+		}
+	}
+}
