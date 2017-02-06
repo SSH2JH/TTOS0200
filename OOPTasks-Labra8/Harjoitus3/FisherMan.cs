7c7b3afd0777318c176df968a460c83254e67949
juuso <juuso@DESKTOP-H8ND6HS>
2017-02-06 09:49:08 +0200

    Started labra8 - task3

---
+++ OOPTasks-Labra8/Harjoitus3/FisherMan.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Tasks {
+	class FisherMan {
+		public string Name { get; set; }
+		public string PNumber { get; set; }
+
+		public override string ToString()
+		{
+			string tmp = string.Format("Name: {0}, Phone number: {1}", Name, PNumber);
+			return tmp;
+		}
+	}
+	class Fish : FisherMan {
+		public string Species { get; set; }
+
+		private float length, weigth;
+		public float Length
+		{
+			get { return length; }
+			set {
+				length = value;
+				if (length < 0) {
+					length = 0.01f;
+				}
+			}
+		}
+		public float Weight
+		{
+			get { return weigth; }
+			set {
+				weigth = value;
+				if (weigth < 0) {
+					weigth = 0.01f;
+				}
+			}
+		}
+		public override string ToString()
+		{
+			string tmp = string.Format("\nFish species: {0}, length: {1}, weigth: {2}", Species, Length, Weight);
+			return base.ToString() + tmp;
+		}
+
+	}
+}
---
+++ OOPTasks-Labra8/Harjoitus3/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: OOP Labra 8 - Tehtävä 3
+ * Kuvaus:
+ *		 Toteuta sovellus, jossa voit hallita kalastukseen liittyviä tietoja.
+ *		 Ohjelman pitää pystyä käsittelemään: saadun kalan perustiedot (laji, pituus ja paino),
+ *		 kalastajan perustiedot (nimi, puhelinnumero) sekä saadun kalapaikan perustiedot (nimi ja paikka).
+ *		 Pohdi tarvittava luokkarakenne UML-kaaviona.
+ *		 Toteuta pääohjelmassa kalastaja ja muutamia kaloja. Tulosta lopuksi kalarekisterin sisältö. 
+ * Pvm: 3.2.2017
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Tasks {
+	class Program {
+		static void Main(string[] args)
+		{
+			FisherSimulator();
+		}
+		static void FisherSimulator()
+		{
+			List<FisherMan> fisher1 = new List<FisherMan>();
+			Fish fish1 = new Fish();
+			fish1.Name = "Bob Fisher";
+			fish1.PNumber = "505 123 1234";
+			
+			Console.WriteLine(fish1.ToString());
+		}
+	}
+}
